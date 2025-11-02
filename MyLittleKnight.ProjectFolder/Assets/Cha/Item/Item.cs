be3240bd6c3cd988fac2c2e4d39a2c4adeb4086e
trst3385 <trst3385@naver.com>
2025-11-02 18:03:26 +0900

    아이템이 나무 뒤에 스폰될때를 위한 로직 추가, 하지만 많은 문제로 원상복구
    나무 뒤에 아이템이 스폰되면 나무의 Layer가 우선이라 아이템이 가려짐.
    그래서 가려질때 검은색으로 아이템을 우선으로 보이게 하려 했지만 실패.
    결국 이전으로 복구하고, 대신 아이템을 나무의 Layer보다 우선으로 보이게 Frist라는 최우선 Sorthing Layer를 추가 후 적용.

--- MyLittleKnight.ProjectFolder/Assets/Cha/Item/Item.cs
+++ MyLittleKnight.ProjectFolder/Assets/Cha/Item/Item.cs
@@ -31,7 +31,6 @@ public class Item: MonoBehaviour
 
         ChangeToBow,//활로 무기 변경 아이템 추가 예시
         ChangeToSword,//검으로 무기 변경 아이템 추가 예시
-
     }
 
     [Header("아이템의 타입, 각 효과들 변수")]
@@ -63,7 +62,7 @@ public class Item: MonoBehaviour
 
         //닿은 오브젝트가 "Player" 태그를 가지고 있는지 확인
         if (!other.CompareTag("Player")) return;
-        
+
         isUsed = true;
 
         PlayerStatsEffects playerStatsEffects = other.GetComponent<PlayerStatsEffects>();
@@ -72,7 +71,7 @@ public class Item: MonoBehaviour
         PlayerShield playerShield = other.GetComponent<PlayerShield>();
 
         //셋 중에 하나라도 유효하면 아이템 사용 시도
-        if(playerStatsEffects != null || attackController != null || playerHealth != null )
+        if (playerStatsEffects != null || attackController != null || playerHealth != null)
             UseItem(playerStatsEffects, attackController, playerHealth, playerShield);
         else
             Debug.LogWarning("아이템을 획득했지만 플레이어에게 필요한 컴포넌트(PlayerStatsEffects, AttackController, PlayerHealth)가 없어!");
@@ -92,15 +91,15 @@ public class Item: MonoBehaviour
             case ItemType.Heal: soundToPlay = healSound; break;
             case ItemType.MoveSpeed: soundToPlay = moveSpeedSound; break;
             case ItemType.ShieldHeal: soundToPlay = shieldSound; break;
-            case ItemType.ChangeToBow: soundToPlay = weaponChangeCtbSound; break; 
+            case ItemType.ChangeToBow: soundToPlay = weaponChangeCtbSound; break;
             case ItemType.ChangeToSword: soundToPlay = weaponChangeCtsSound; break;
         }
         if (soundToPlay != null)//아이템 오브젝트가 사라지더라도 사운드는 들리게
-        {        
+        {
             GameObject soundObject = new GameObject("OneShotAudio");//코드가 실행될 때마다 생성되는 오브젝트야,
                                                                     //코드 안에서 만들어진 오브젝트지
-            //이 코드가 실행되면 씬에 "OneShotAudio"라는 이름의 빈 오브젝트가 하나 만들어져.
-            //이 오브젝트는 소리를 재생하는 역할만 하고, 소리 재생이 끝나면 자동으로 사라져.
+                                                                    //이 코드가 실행되면 씬에 "OneShotAudio"라는 이름의 빈 오브젝트가 하나 만들어져.
+                                                                    //이 오브젝트는 소리를 재생하는 역할만 하고, 소리 재생이 끝나면 자동으로 사라져.
 
             soundObject.transform.position = transform.position;//soundObject의 위치를 아이템이 있던 위치(transform.position)와 같게 설정해.
                                                                 //이렇게 하면 소리가 아이템이 있던 곳에서 나는 것처럼 들려.
@@ -110,26 +109,26 @@ public class Item: MonoBehaviour
             tempAudioSource.spatialBlend = 0;//spatialBlend의 0이 2D 사운드, 1이 3D 사운드
             //2D 사운드는 카메라와의 거리에 상관없이 항상 같은 크기로 들리기 때문에, 아이템을 먹을 때마다 소리 크기가 달라지는 문제를 해결
             tempAudioSource.Play();
-            
+
             //소리 재생이 끝난 뒤에 자동으로 파괴되도록
             //소리가 재생되는 시간만큼 기다렸다가, 소리 재생이 끝나면 soundObject를 자동으로 파괴
             Destroy(soundObject, soundToPlay.length);
         }
         //아이템은 한번만 먹고 사라져야해, 이걸 지우면 아이템을 먹어도 사라지지 않아.
-        Destroy(gameObject);     
+        Destroy(gameObject);
     }
-    
+
     void UseItem(PlayerStatsEffects statsEffects, AttackController attackController, PlayerHealth playerHealth, PlayerShield playerShield)
     //실제 아이템 효과를 적용하는 함수. itemType에 따라 다른 효과를 줘.
     {
-        switch(itemType)//itemType(Inspector에서 설정한 값)에 따라 분기한다.
+        switch (itemType)//itemType(Inspector에서 설정한 값)에 따라 분기한다.
         {
             case ItemType.None:
                 Debug.LogWarning("아이템 타입이 설정되지 않았습니다! Inspector를 확인하세영!");
                 break;
 
             case ItemType.ArrowPower://활 데미지 증가
-                if(statsEffects != null)
+                if (statsEffects != null)
                     statsEffects.ArrowDamageUp(EffectDamage, AttackCooldown);
                 break;
 
@@ -144,7 +143,7 @@ public class Item: MonoBehaviour
                 break;
 
             case ItemType.ShieldHeal://방어력 회복
-                if(statsEffects != null)
+                if (statsEffects != null)
                     playerShield.HealShield(ShieldAmount);//PlayerShield의 HealShield 함수 호출
                 break;
 
