408b5c511dce3b329e21cfaaa904e6646ccb5d47
trst3385 <trst3385@naver.com>
2025-11-02 17:58:05 +0900

    유니티 업데이트로 불필요한 Using 삭제
    Enemy 스크립트의 using Unity.Android.Gradle; , using Unity.Properties; 삭제. (Windows로 빌드를 하니 불필요)

--- MyLittleKnight.ProjectFolder/Assets/Cha/Enemy/Enemy.cs
+++ MyLittleKnight.ProjectFolder/Assets/Cha/Enemy/Enemy.cs
@@ -2,8 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Transactions;
-using Unity.Android.Gradle;
-using Unity.Properties;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
