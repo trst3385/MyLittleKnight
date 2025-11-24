901ff3992b1c70c374544f53f396c6fd0036e760
trst3385 <trst3385@naver.com>
2025-11-24 17:17:03 +0900

    if문 간략화
    if, else if문을 한줄로 간략화

--- MyLittleKnight.ProjectFolder/Assets/Cha/Scenes/MainScene/Player/MainMenuArrow.cs
+++ MyLittleKnight.ProjectFolder/Assets/Cha/Scenes/MainScene/Player/MainMenuArrow.cs
@@ -31,13 +31,7 @@ public class MainMenuArrow: MonoBehaviour
         //화살이 어떤 오브젝트에 닿으면(예: 몬스터, 배경) 그냥 사라지게만 처리
 
         //몬스터와 충돌했을 때 (옵션: 몬스터 태그가 있다면)
-        if (other.CompareTag("Enemy"))
-        {
-            Destroy(gameObject);
-        }
-        else if (other.CompareTag("Ground"))//그 외 배경/바닥에 충돌했을 때 (옵션: Ground 태그가 있다면)
-        {
-            Destroy(gameObject);
-        }
+        if (other.CompareTag("Enemy")) Destroy(gameObject);
+        else if (other.CompareTag("Ground")) Destroy(gameObject);//그 외 배경, 바닥에 충돌했을 때 (옵션: Ground 태그가 있다면)
     }
 }
