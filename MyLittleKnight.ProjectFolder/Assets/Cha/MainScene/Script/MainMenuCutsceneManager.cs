04dd5fe812d415246078fb8e2ca8a2ddfb35d237
trst3385 <trst3385@naver.com>
2025-11-26 17:18:03 +0900

    프로젝트 에셋 폴더 구조 정리
    - Assets/Cha 폴더 내 모든 리소스를 씬별로 분리하여 MainScene 및 GameScene 폴더 아래에 재배치하여 프로젝트 가독성을 개선.
    - 몬스터 난이도 상승 체감을 높이기 위해 SpeedIncreaseRatio 값을 0.5f로 변경했으나, 심한 떨림(Jittering) 현상 발생으로 임시로 0.15f로 조정.
    - Windows 빌드 시 발생하는 'Unity.Android.Gradle' 네임스페이스 참조 오류를 수정하여 빌드 파이프라인을 복구.

--- MyLittleKnight.ProjectFolder/Assets/Cha/MainScene/Script/MainMenuCutsceneManager.cs
+++ MyLittleKnight.ProjectFolder/Assets/Cha/MainScene/Script/MainMenuCutsceneManager.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.ConstrainedExecution;
-using Unity.Android.Gradle;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SocialPlatforms;
