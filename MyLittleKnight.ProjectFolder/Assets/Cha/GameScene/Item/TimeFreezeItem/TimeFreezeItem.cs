2208e9c460688c21ab8022b061d4dbe6ed4f6a60
trst3385 <trst3385@naver.com>
2025-12-10 14:14:52 +0900

    TimeFreezeItem.cs (아이템 획득):
    - [SerializeField] private AudioClip collectSound 변수 추가 및 프리팹에 사운드 연결 완료.
    - OnTriggerEnter2D에 SoundManager.Instance.PlaySFX(collectSound) 호출 로직 추가해 오브젝트가 파괴되어도 획득 시 사운드를 들릴 수 있게 수정.

--- MyLittleKnight.ProjectFolder/Assets/Cha/GameScene/Item/TimeFreezeItem/TimeFreezeItem.cs
+++ MyLittleKnight.ProjectFolder/Assets/Cha/GameScene/Item/TimeFreezeItem/TimeFreezeItem.cs
@@ -2,9 +2,12 @@
 
 public class TimeFreezeItem : MonoBehaviour
 {
-
     [Header("시간 정지 지속 시간")]
     [SerializeField] private float freezeDuration = 3.0f;//시간이 멈췄을때 지속시간
+
+    [Header("사운드 설정")]
+    [SerializeField] private AudioClip pickupSound;//아이템 획득 시 들릴 사운드
+
 
     private TimeFreezeItemSpawner TimeFreezeitemSpawner;//TimeFreezeItemSpawner 스크립트 참조
 
@@ -23,6 +26,10 @@ public class TimeFreezeItem : MonoBehaviour
             if (TimeFreeze.Instance != null)
             {
                 TimeFreeze.Instance.ActivateTimeFreeze(freezeDuration);
+
+                //획득 사운드 재생 로직, SoundManager에게 이 사운드를 한 번 재생해 달라고 요청
+                if (SoundManager.Instance != null && pickupSound != null) SoundManager.Instance.PlaySFX(pickupSound);
+
 
                 //TimeFreezeitemSpawner에게 파괴 알림
                 if (TimeFreezeitemSpawner != null) TimeFreezeitemSpawner.ItemDestroyed();//인수를 넘길 필요가 없어짐
