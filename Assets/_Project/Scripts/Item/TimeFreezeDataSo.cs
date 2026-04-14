aba328337e80df0d9b81b229a57b7a41e75355ba
trst3385 <trst3385@naver.com>
2026-04-14 15:37:58 +0900

    feat: 시간 정지 SO 적용 및 보스 몬스터 설계 구조 문서화
    - 시간 정지 시스템 데이터 관리 최적화 (ScriptableObject).
    1. 기존 TimeFreezeItem의 하드코딩된 데이터(지속 시간, 획득 사운드)를 SO로 분리
    2. [TimeFreezeData] SO 추가로 데이터와 로직을 분리하여 기획 수치 변경 시 코드 수정 불필요
    3. TimeFreezeItem에서 SO 참조 및 안전장치(Null Check) 추가
    
    - Git의 README에 보스 몬스터 설계 과정, 문제 해결 및 배운 점 상세 기록.
    1. '개방-폐쇄 원칙(OCP)'을 적용하여 기존 Enemy 시스템을 건드리지 않고 기능을 확장한 사례 기록
    2. 다형성을 활용한 해결 방안 및 핵심 기술 성과 문서화
    3. 상태 이상(슬로우, 시간 정지) 면역 시스템을 메서드 재정의로 구현 (OCP 원칙 준수)

---
+++ Assets/_Project/Scripts/Item/TimeFreezeDataSo.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewTimeFreezeData", menuName = "Item/TimeFreezeData")]
+public class TimeFreezeData : ScriptableObject
+{
+    [Header("시간 정지 설정")]
+    public float freezeDuration = 5f;//시간정지 지속 시간
+
+    [Header("사운드 설정")]
+    public AudioClip pickupSound;//아이템 획득 시 사운드
+}
---
+++ Assets/_Project/Scripts/Item/TimeFreezeItem.cs
@@ -0,0 +1,33 @@
+﻿using Unity.VisualScripting.Antlr3.Runtime.Misc;
+using UnityEngine;
+
+public class TimeFreezeItem : MonoBehaviour
+{
+    [SerializeField] private TimeFreezeData stats;//TimeFreezeData SO 인스펙터에서 연결
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (stats == null)//stats (SO에셋)가 연결되어 있는지 확인하는 안전장치
+        {
+            Debug.LogWarning("TimeFreezeItem: Stats(SO)가 연결되지 않았어!");
+            return;
+        }
+
+        if (other.CompareTag("Player") && TimeFreeze.Instance != null)
+        {
+            //직접 숫자를 쓰는 대신 이젠 stats(SO)에서 가져옴
+            TimeFreeze.Instance.ActivateTimeFreeze(stats.freezeDuration);
+
+            //사운드도 SO에서 가져옴, (아이템은 획득 시 사라지니 획득 시 사운드는 SoundManager에서 관리)
+            if (SoundManager.Instance != null && stats.pickupSound != null)
+            {
+                SoundManager.Instance.PlaySFX(stats.pickupSound);
+            }
+
+            if (TimeFreezeItemSpawner.Instance != null)
+            {
+                TimeFreezeItemSpawner.Instance.OnItemPickedUp();
+            }
+        }
+    }
+}
